65a0389ee3de9dcf700a38fc165669b151358455
Natan <Th3than@gmail.com>
2023-02-10 16:29:55 +0700

    lab17-1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,48 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void randData(double *a,int n,int m){
+	for (int i = 0; i < n*m; i++)
+	{
+		*(a+i) =(rand()%101)/100.0;
+	}
+	
+}
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout <<*(a+i);
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+void findRowSum(const double *a,double *an,int n,int m){
+	double s = 0;
+	int i = 0;
+	for (int p = 0; p < n*m; p++)
+	{
+		s += *(a+p);
+		if (p%m == m-1)
+		{
+			an[i] = s;
+			i++;
+			s = 0;
+		}
+		
+    }
+	
+}
+void findColSum(const double *a,double *am,int n,int m){
+	for (int i = 0; i < n*m; i++)
+	{
+		am[i%m] = 0;
+	}
+	
+	for (int p = 0; p < n*m; p++)
+	{
+		am[p%m] += *(a+p); 
+		
+    }
+	
+}
